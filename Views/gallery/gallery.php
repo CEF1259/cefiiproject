f68efb7ce7273df1afbcbe99a534ec9524ef9e7a
CEF1259 <michael.legrain@hotmail.com>
2024-12-13 12:39:15 +0100

    Update gallery.php

--- Views/gallery/gallery.php
+++ Views/gallery/gallery.php
@@ -3,6 +3,8 @@
 
 $title = "gallery par des enfants";
 
+echo "<h2>Soumissons de nos utilisateurs</h2>";
+
 foreach($list as $value) {
      echo "<td><img src='$value->image' class='picture' alt='picture'"; 
 }
