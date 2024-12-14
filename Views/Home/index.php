ef61aa6da4e6e7a4bd7bad130614d0e14729d765
CEF1259 <michael.legrain@hotmail.com>
2024-12-15 00:02:57 +0100

    Update index.php

--- Views/Home/index.php
+++ Views/Home/index.php
@@ -2,8 +2,11 @@
         <?php $title = "Page d'accueil"?>
         <h2>Bonjour au site de recettes MJC</h2>
         <div class="paragraph">
-        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-        sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Excepteur sint occaecat cupidatat non proident, 
+            sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
         </div>
         <img src="images/artchildcooking.jpg" alt="picture">
     </div>
